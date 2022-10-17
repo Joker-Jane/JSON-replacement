ff63b5792183231014e5e9cc4bd13a20441aec77
Duncan <1599092341@qq.com>
2022-10-17 10:49:37 -0500

    Supports line-by-line mode

--- json_replace.go
+++ json_replace.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"errors"
 	"flag"
@@ -8,6 +9,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -25,6 +27,7 @@ type Rule struct {
 var inputPath *string
 var outputPath *string
 var configPath *string
+var lineByLine *bool
 
 // The list of all rules
 var rules []Rule
@@ -37,7 +40,10 @@ var startTime time.Time
 This program reads file(s) containing JSON records, and redacts or replaces the
 private / client information in each based on some predefined parameters.
 
-All three flags must be specified.
+-i, -o, and -c flags must be specified.
+
+Reading multiple JSON objects line-by-line is supported by specifying -l flag.
+Note that a single JSON object in multiple lines is not supported if line-by-line mode is enabled.
 
 Usage:
 
@@ -53,12 +59,16 @@ Flags:
 
 	-c config_path
 		Set the path to the config file. The file must be a json file.
+
+	-l
+		Read multiple json objects line by line.
 */
 func main() {
 	// Config and parse flags
 	inputPath = flag.String("i", "", "input path")
 	outputPath = flag.String("o", "", "output path")
 	configPath = flag.String("c", "", "config path")
+	lineByLine = flag.Bool("l", false, "line-by-line mode")
 
 	flag.Parse()
 
@@ -69,15 +79,18 @@ func main() {
 		log.Fatal("Usage: ./json_replace -i input -o output -c config")
 	}
 
-	// Check if input and output types match
-	if filepath.Ext(*inputPath) != filepath.Ext(*outputPath) {
-		log.Fatal("Error: Input and output paths must either be both files or folders")
-	}
+	// Unnecessary
+	/*
+		// Check if input and output types match
+		if filepath.Ext(*inputPath) != filepath.Ext(*outputPath) {
+			log.Fatal("Error: Input and output paths must either be both files or folders")
+		}
 
-	// Check if input and output types are json or directory
-	if filepath.Ext(*inputPath) != ".json" && filepath.Ext(*inputPath) != "" {
-		log.Fatal("Error: Input and output paths must be either files or folders")
-	}
+		// Check if input and output types are json or directory
+		if filepath.Ext(*inputPath) != ".json" && filepath.Ext(*inputPath) != "" {
+			log.Fatal("Error: Input and output paths must be either files or folders")
+		}
+	*/
 
 	// Check if input path exists
 	_, err := os.Stat(*inputPath)
@@ -118,7 +131,7 @@ func main() {
 		return err
 	})
 	if err != nil {
-		log.Fatal("Error: Failed to wal through the input directory")
+		log.Fatal("Error: Failed to walk through the input directory")
 	}
 
 	log.Printf("Success: Processed %d file(s) in %.4f second(s)\n", fileCounter, time.Since(startTime).Seconds())
@@ -126,32 +139,33 @@ func main() {
 
 // Handle input json file
 func handleFile(filePath string) {
-	// Read and parse input file
+	// Read input file
 	input, err := os.ReadFile(filePath)
 	if err != nil {
-		log.Fatal("Error: Cannot read config file '" + filePath + "'")
+		log.Fatal("Error: Cannot read input file '" + filePath + "'")
 	}
 
-	var m interface{}
-	err = json.Unmarshal(input, &m)
-	if err != nil {
-		log.Fatal("Error: File '" + filePath + "' is not in valid json format")
-	}
+	// Store the result
+	var result []byte
 
-	// Apply every rule on files
-	for _, r := range rules {
-		switch r.Type {
-		case "per-field":
-			process("", m, r.Original, r.Replacement, r.FieldName, false)
-		case "global":
-			process("", m, r.Original, r.Replacement, r.FieldName, true)
-		default:
-			log.Fatal("Error: Invalid type '" + r.Type + "'")
+	// If is in line-by-line mode, split the input by \n, process each line, and append to result
+	// If not, store the result directly
+	if *lineByLine {
+		inputs := bytes.Split(input, []byte("\n"))
+		for l, i := range inputs {
+			r, err := handleJSON(i)
+			if err != nil {
+				log.Fatal("Error: Line " + strconv.Itoa(l+1) + " of '" + filePath + "' is not in valid JSON format")
+			}
+			r = append(r, byte('\n'))
+			result = append(result, r...)
+		}
+	} else {
+		result, err = handleJSON(input)
+		if err != nil {
+			log.Fatal("Error: File '" + filePath + "' is not in valid JSON format")
 		}
 	}
-
-	// Write file to output
-	result, _ := json.Marshal(m)
 
 	// Get target output path
 	target := strings.Replace(filePath, *inputPath, *outputPath, 1)
@@ -176,6 +190,37 @@ func handleFile(filePath string) {
 	fileCounter++
 }
 
+// Handle a single JSON object
+func handleJSON(input []byte) ([]byte, error) {
+	// Return if the input is empty
+	if len(input) == 0 {
+		return nil, nil
+	}
+
+	// Parse input file
+	var m interface{}
+	err := json.Unmarshal(input, &m)
+	if err != nil {
+		return nil, err
+	}
+
+	// Apply every rule on files
+	for _, r := range rules {
+		switch r.Type {
+		case "per-field":
+			process("", m, r.Original, r.Replacement, r.FieldName, false)
+		case "global":
+			process("", m, r.Original, r.Replacement, r.FieldName, true)
+		default:
+			log.Fatal("Error: Invalid type '" + r.Type + "'")
+		}
+	}
+
+	// Write file to output
+	result, _ := json.Marshal(m)
+	return result, nil
+}
+
 // Process non-string elements
 func process(k string, v interface{}, from string, to string, field string, isGlobal bool) {
 	switch v.(type) {
@@ -189,7 +234,7 @@ func process(k string, v interface{}, from string, to string, field string, isGl
 // Process maps
 func processMap(m map[string]interface{}, from string, to string, field string, isGlobal bool) {
 	// If global rule applies, iterate every element in the map
-	// If not, check if the particular field exits
+	// If not, check if the particular field exists
 	if isGlobal {
 		for k, v := range m {
 			switch v.(type) {
